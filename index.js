1908d3a3d84cf36d0503c9f6cfb2407b927f4b91
Miguh Ruiz <miguhruiz@gmail.com>
2017-08-10 16:54:01 +0200

    [Update] Database URL

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const mongoose = require('mongoose')
 const app = express()
 
 const port = process.env.PORT || '3000'
+const mongo_url = process.env.DATABASE || 'mongodb://localhost:27017/names_api'
 
-mongoose.connect('mongodb://localhost:27017/names_api')
+mongoose.connect(mongo_url)
 
 app.use(bodyParser())
 
