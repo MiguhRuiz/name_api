81b2028b29bec11d7cb6dd80e3075cadf3483ff0
Miguh Ruiz <miguhruiz@gmail.com>
2017-08-10 16:24:37 +0200

    [Add] Body Parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 const port = process.env.PORT || '3000'
+
+app.use(bodyParser())
 
 app.get('/', (req, res) => {
     res.send('Hello World')
